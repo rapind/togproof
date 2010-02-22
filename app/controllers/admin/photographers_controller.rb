b640f252d20e7132247596338ec700fa4bb4d630
Dave Rapin <daverapin@dave-rapins-macbook-pro.local>
2010-02-22 09:08:40 -0500

    disabled settings changes in demo mode

--- app/controllers/admin/photographers_controller.rb
+++ app/controllers/admin/photographers_controller.rb
@@ -2,6 +2,7 @@ class Admin::PhotographersController < Admin::HomeController
   inherit_resources
   actions :edit, :update
   before_filter :load_photographer
+  before_filter :check_demo_mode, :only => :update
   
   # redirect to edit path on update instead of show
   def update
@@ -13,6 +14,13 @@ class Admin::PhotographersController < Admin::HomeController
   
   def load_photographer
     @photographer = Photographer.find :first
+  end
+  
+  def check_demo_mode
+    if defined?(DEMO_MODE) and DEMO_MODE
+      flash[:notice] = 'Cannot modify the settings in demo mode.'
+      redirect_to edit_admin_photographer_path
+    end
   end
   
 end
