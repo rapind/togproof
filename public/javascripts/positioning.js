8094129fed385346b7c97d7d983482b0df56691e
Dave Rapin <daverapin@dave-rapins-macbook-pro.local>
2010-03-05 17:38:45 -0500

    Added product management

--- public/javascripts/positioning.js
+++ public/javascripts/positioning.js
@@ -49,4 +49,48 @@ $(document).ready(function() {
       revert: true
   });
 
+
+	/* ***
+	** TABLE ROW ORDERING VIA DRAG AND DROP **
+	** requires: 
+	**  1) AUTH_TOKEN should be set to rails form auth token in the calling erb page.
+	**  2) You need an update_position action and route on the controller for the object being re-ordered.
+	**     This action should return a json object.
+	**  3) There should be a header row (assumption is made that the first row is 1 and not 0).
+	**  4) The table you want to sort should have a class assignment of "sortable".
+	*/
+  $("table.sortable").tableDnD({
+		onDragClass: "row_drag",
+    onDrop: function(table, row) {
+			// figure out where the row moved to
+      var position = 0;
+      var rows = table.tBodies[0].rows;
+      for (var i=0; i<rows.length; i++) {
+        if (rows[i].id === row.id) {
+          position = i;
+        }
+      }
+      // submit the id and position to the server via ajax
+      $.post(document.location + '/' + row.id + '/update_position', {_method:"put", authenticity_token:AUTH_TOKEN, position:position}, function(data) {
+        json = eval('(' + data + ')');
+				var icon = '/images/icons/notice.png';
+				if (json.title === "Error") icon = '/images/icons/warning.png';
+        // inform the user of success / fail via gritter (growl)
+        $.gritter.add({
+      		title: json.title,
+      		text: json.message,
+      		image: icon,
+      		time: 3000
+      	});
+      });
+    },
+		dragHandle: "dragHandle"
+  });
+
+	$("table.sortable tr").hover(function() {
+		$(this.cells[0]).addClass('showDragHandle');
+	}, function() {
+		$(this.cells[0]).removeClass('showDragHandle');
+	});
+	
 });
