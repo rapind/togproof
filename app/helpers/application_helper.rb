dc39e954556daeab1b1fced6bc522c4dd79b7796
Dave Rapin <daverapin@rapin.local>
2010-04-27 09:49:46 -0400

    changed buttons over to more IE compatible ones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,32 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
-  # to add a css class to the body tag of each page equal to the controller name
-  # you can add additional classes to the body tag in your yielded view using
-  # <% body_classes << 'anotherclass' %>
-  def body_classes
-    @body_classes ||= [controller.controller_name]
-  end
-  
-  def display_flash
+  def display_flash   
     html = ''
+    # if we get double flash messages, remove one of them. For some reason inherited resources decided to add a new flash message.
+    flash.discard(:success) if flash[:notice] and flash[:success]
     if flash[:notice] or flash[:warning] or flash[:error]
       html = "<script type=\"text/javascript\">\n$(document).ready(function() {\n"
       flash.each do |key,value|
         if !value.blank?
-          html = "#{html}$.gritter.add({ \ntitle: \"#{key.to_s.capitalize}\", \ntext: \"#{value}\", \nimage: \"/images/icons/#{key.to_s}.png\", \ntime: 3000 \n});\n"
+          if controller.controller_name == 'products' and controller.action_name == 'show'
+            html = "#{html}\n$('.flash_notice').show();\n$('.flash_notice span').html('#{value.gsub('"', '\\"')}');"
+          else
+            html = "#{html}$.gritter.add({ \ntitle: \"#{key.to_s.capitalize}\", \ntext: \"#{value.gsub('"', '\\"')}\", \nimage: \"/images/icons/#{key.to_s}.png\", \ntime: 3000 \n});\n"
+          end
         end
       end
       html = "#{html}});\n</script>"
     end
+    return html
   end
   
-  def build_button(name, class_name, form)
-    if request.env['HTTP_USER_AGENT'].include?('MSIE 6')
-      if form
-        return form.submit(name)
-      else
-        return submit_tag(name)
-      end
-    else
-      return "<button class='#{class_name}'><span>#{name}</span></button>"
-    end
+  def build_submit_button(title)
+    "<button type='submit' class='button positive'> #{image_tag('icons/positive.png')} #{title} </button>"
+  end
+  
+  def build_link_button(title, path)
+    "<p class='buttons'>#{link_to(image_tag('icons/positive.png') + ' ' + title, path, :class => 'button positive')}</p>"
   end
   
 end
--- app/helpers/photo_comments_helper.rb
+++
@@ -1,2 +0,0 @@
-module PhotoCommentsHelper
-end
