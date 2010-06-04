1885d63b6a743887fd57db6be4821007a3e31dba
Luca G. Soave <luca.soave@gmail.com>
2010-06-04 11:27:40 +0200

    set theme to default cause default_with_flash doesn't work

--- config/initializers/grokphoto.rb
+++ config/initializers/grokphoto.rb
@@ -4,17 +4,21 @@
 # session store
 # TODO - change your key and secret values
 ActionController::Base.session = {
-  :key         => '_yourappname_session',
-  :secret      => 'your_secret_key_should_replace_this'
+  :key         => '_lauranovara.com_session',
+  :secret      => 'a0as9d8f7grtt655dg3j2l12k3j4h5yh5jjqk2ldod09ccvv7b7b6v55xa5attdtsugg7743h454h5666klnbbafor8f494uhpfin4894h94q38ghf570gf07489qrh774f43f'
 }
+
+# for Heroku ( but doesn't work )
+#ActionController::Base.page_cache_directory = "#{RAILS_ROOT}/tmp/cache"
 
 # paperclip / imagemagick 
 # TODO - change this to where imagemagick is installed (which convert)
+#Paperclip.options[:command_path] = "/usr/local/bin"
 Paperclip.options[:command_path] = "/usr/bin"
 
 # mailer
 # TODO - change this to your host
-ActionMailer::Base.default_url_options[:host] = "localhost"
+ActionMailer::Base.default_url_options[:host] = "lauranovara.webbyapp.com"
 
 # gmail smtp
 # TODO - change the username and password to your gmail account settings
@@ -23,8 +27,8 @@ ActionMailer::Base.smtp_settings = {
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true,
-  :user_name => "username@gmail.com",
-  :password => "yourpassword"
+  :user_name => "novara.laura@gmail.com",
+  :password => "lauratempura"
 }
 
 # memcache store - TODO uncomment this if you're using a memcache and change your namespace if you like
