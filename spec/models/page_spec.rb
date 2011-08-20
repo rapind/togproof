4d5aeb694b5c516e34f6bc557c2154d8b4c8dc03
Dave Rapin <dave@rapin.com>
2011-08-20 13:47:42 -0400

    filled out portfolio model spec

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -20,4 +20,10 @@ describe Page do
   it { should ensure_length_of(:keywords).is_at_least(3).is_at_most(200) }
   it { should ensure_length_of(:summary).is_at_least(5).is_at_most(250) }
 
+  context "to_param" do
+    it "should return an SEO friendly slug" do
+      @page.to_param.should eql("#{@page.id}-#{@page.title.parameterize}")
+    end
+  end
+
 end
--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -2,4 +2,23 @@ require 'spec_helper'
 
 describe Portfolio do
 
+  before(:all) do
+    # We need an existing object for uniqueness validation tests.
+    @portfolio = Portfolio.create( :title => 'Fake Title' )
+  end
+
+  # ****
+  # Validations
+  it { should validate_presence_of(:title) }
+  it { should validate_uniqueness_of(:title) }
+  it { should ensure_length_of(:title).is_at_least(2).is_at_most(64) }
+  it { should ensure_length_of(:keywords).is_at_least(3).is_at_most(200) }
+  it { should ensure_length_of(:description).is_at_least(5).is_at_most(2000) }
+
+  context "to_param" do
+    it "should return an SEO friendly slug" do
+      @portfolio.to_param.should eql("#{@portfolio.id}-#{@portfolio.title.parameterize}")
+    end
+  end
+
 end
