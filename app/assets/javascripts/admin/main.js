50cc9f5c456f50835548bab4e57de5265ab2fd47
Dave Rapin <dave@rapin.com>
2011-09-20 14:34:53 -0400

    commented out fancybox and datepicker temporarily so we dont get constant recursive deadlock errors with sprocket pre-processing.

--- app/assets/javascripts/admin/main.js
+++ app/assets/javascripts/admin/main.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 
 	// Lightbox effect - overlay the title over the bottom left of the image.
-	$("a[rel='lightbox']").fancybox({
-	  titlePosition: 'inside'
-	});
+	// $("a[rel='lightbox']").fancybox({ titlePosition: 'inside' });
 	
 	// date picker
-	$(".date_picker").datepicker({ dateFormat: 'MM dd, yy' });
+	// $(".date_picker").datepicker({ dateFormat: 'MM dd, yy' });
 
 });
