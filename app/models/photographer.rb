87e9883c101cf8dfa29b9203d860bf79363b06dc
Dave Rapin <dave@rapin.com>
2012-03-12 17:08:30 -0400

    fixed the settings form

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -7,21 +7,17 @@ class Photographer < ActiveRecord::Base
   # ****
   # Validation
   validates :name, :presence => true, :length => { :within => 3..100 }
-  validates :description, :presence => true, :length => { :within => 10..8000 }
+  validates :description, :length => { :within => 10..8000, :allow_blank => true }
   validates :phone, :length => { :within => 7..20, :allow_blank => true }
-  validates :blog_url, :length => { :within => 10..255, :allow_blank => true }
+  validates :portfolio_url, :length => { :within => 10..512, :allow_blank => true }
+  validates :blog_url, :length => { :within => 10..512, :allow_blank => true }
   validates :facebook_url, :length => { :within => 10..255, :allow_blank => true }
   validates :twitter_url, :length => { :within => 10..255, :allow_blank => true }
-  validates :disqus_short_name, :length => { :within => 2..64, :allow_blank => true }
   validates :conversion_code, :length => { :within => 10..9000, :allow_blank => true }
 
   # ****
   # Mass-assignment protection
-  attr_accessible :email, :password, :logo, :name, :description, :phone, :blog_url, :facebook_url, :twitter_url, :google_analytics_key, :disqus_short_name, :conversion_code, :remember_me
-
-  # ****
-  # Attachments
-  mount_uploader :logo, LogoUploader
+  attr_accessible :email, :password, :name, :description, :phone, :portfolio_url, :blog_url, :facebook_url, :twitter_url, :google_analytics_key, :conversion_code, :remember_me
 
   # Make sure we always have an auth token assigned.
   before_save :ensure_authentication_token
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,10 @@ p = Photographer.create(
   :name => 'Dave Rapin Photography',
   :email => 'dave@grokphoto.org',
   :password => 'password',
-  :description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  :description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   :blog_url => 'http://rapin.com',
   :twitter_url => 'http://twitter.com/#!/rapind',
-  :google_analytics_key => 'UA-2450369-23',
-  :logo => File.new(Rails.root.join('creatives', 'logo.png'))
+  :google_analytics_key => 'UA-2450369-23'
 )
 
 puts "Creating galleries"
