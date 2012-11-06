649f3cec335b7254f2a609f3b159daf418d213c4
Dave Rapin <dave@rapin.com>
2012-11-06 18:55:19 -0500

    moved contact request emails to the controller

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -8,7 +8,13 @@ class ContactRequestsController < ApplicationController
   def create
     @contact_request = ContactRequest.create(params[:contact_request])
     if @contact_request.valid?
-      respond_with(@contact_request, :location => thanks_contact_requests_path)
+      # Send the email
+      begin
+        ContactRequestMailer.contact(@contact_request).deliver
+        respond_with(@contact_request, :location => thanks_contact_requests_path)
+      rescue Exception => e
+        redirect_to root_path, :alert => "There was a problem submitting a contact request. Please contact me directly at: #{Photographer.cached.email}"
+      end
     else
       respond_with @contact_request
     end
--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -13,23 +13,12 @@ class ContactRequest < ActiveRecord::Base
   scope :read, where(:read => true)
 
   # Logging
-  after_create :log_create_event, :send_email
+  after_create :log_create_event
 
   private #----
 
     def log_create_event
       Event.create(:description => "Contact request from: #{email}")
     end
-
-    def send_email
-      # Send the email
-      begin
-        ContactRequestMailer.contact(self).deliver
-
-        redirect_to root_path, :notice => 'Contact request sent.'
-      rescue Exception => e
-        render :invite, :error => "The was a problem submitting a contact request. Please contact me directly at: #{Photographer.cached.email}"
-      end
-    end
-
+    
 end
