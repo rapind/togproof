5ebaf68407ab9e0d110e5f6cb2132b78320f906b
Dave Rapin <dave@rapin.com>
2013-06-16 18:42:01 -0400

    fixed specs, for what it's worth

--- spec/models/gallery_spec.rb
+++ spec/models/gallery_spec.rb
@@ -8,12 +8,12 @@ describe Gallery do
 
   context "validations" do
     it { should validate_presence_of(:name) }
-    it { should ensure_length_of(:name).is_at_least(2).is_at_most(20) }
+    it { should ensure_length_of(:name).is_at_least(2).is_at_most(30) }
     it { should validate_uniqueness_of(:name) }
     
     it { should ensure_length_of(:keywords).is_at_least(2).is_at_most(255) }
     
-    it { should ensure_length_of(:description).is_at_least(10).is_at_most(8000) }
+    it { should ensure_length_of(:description).is_at_least(10) }
   end
   
 end
